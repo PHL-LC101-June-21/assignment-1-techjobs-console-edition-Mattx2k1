1c35d07ad5e3b77e9beb0ec43edb1f3395169e9f
Mattx2k1 <44537080+Mattx2k1@users.noreply.github.com>
2021-09-28 21:38:19 -0400

    Updated code; working so far using an if statement

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -119,7 +119,10 @@ public class TechJobs {
 
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
-
-        System.out.println("printJobs is not implemented yet");
+        if (someJobs.size() == 0) {
+            System.out.println("No results returned");
+        } else {
+            System.out.println("still implementing");
+        }
     }
 }
